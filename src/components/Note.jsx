7fbe8c16a29b7ca2c265916a1bfe71e7c7b987c4
Miguel <losky321@hotmail.com>
2022-07-26 18:26:29 -0400

    React Course Project, Keeper App First Commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note () {
+    
+    return (
+    <div draggable="true" className="note">
+        <h1>Tittle</h1>
+        <p>Content</p>
+    </div>
+  );
+}
+
+export default Note;
